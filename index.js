f8a43d85061cbafee427783161fd98422fbeb875
MesWinDu <math7064@gmail.com>
2024-03-23 19:48:57 +0700

    Update 1.8

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ app.post('/test',(req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on port ${PORT}`);
 });
