730229e7efc559689746342f940b784020264893
MesWinDu <math7064@gmail.com>
2024-03-24 17:43:21 +0700

    Update 2.4 HOTFIX 2

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get("/api/fetchdatapresenint", (req, res) => {
                     counts.push(row.count);
                 }, () => {
                     // Once all rows have been retrieved, send the response
-                    res.json({ "FirstName": ["6:00","6:30","7:00","7:30","8:00","8:30","9:00","9:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00" ], "Total": counts });
+                    res.json({ "FirstName": intervals, "Total": counts });
                     console.log("Data sent:", intervals, counts);
 
                     // Close the database connection
